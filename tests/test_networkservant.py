6dc5c9f5527aba18ca634854a7ff18dcc622da2a
Igor Ovsyannikov <kamish@outlook.com>
2019-08-15 15:45:01 +0300

    Initial commit

---
+++ tests/test_networkservant.py
@@ -0,0 +1,7 @@
+from networkservant import __version__
+import toml
+
+def test_version():
+    with open("pyproject.toml") as fp:
+        toml_version = toml.load(fp)["tool"]["poetry"]["version"]
+    assert __version__ == toml_version
